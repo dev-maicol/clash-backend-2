01abf79be6a8b9c76c6b4e140482d52355a9bcb0
Maicol <maicol.oficial.93@gmail.com>
2025-02-25 23:23:18 -0400

    Modifyyyyy

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
+require('dotenv').config(); // Cargar variables de entorno
 const port = process.env.PORT || 3000
 
 const API_KEY = process.env.API_KEY_COC
@@ -17,30 +18,49 @@ app.get('/', (req, res) => {
 
 // api para clash of clans
 app.get('/v1/clans/war/:tag', async (req, res) => {
-  try{
+  try {
     const tag = req.params.tag
-    
-    
     const url = URL_BASE + 'clans/%23' + tag + '/currentwar'
-    console.log({url})
-    console.log({API_KEY})
+    // console.log({ url })
+    // console.log({ API_KEY })
     const response = await fetch(url, {
       headers: {
         'Authorization': `Bearer ${API_KEY}`,
       }
     })
-    console.log(response);
-    
+    // console.log(response);
+
     if (!response.ok) {
       throw new Error(`Error en la API: ${response.status}`)
     }
     const data = await response.json()
     res.json(data)
 
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+
+})
+
+app.get('/v1/clans/capital/:tag', async (req, res) => {
+  try {
+    const tag = req.params.tag
+    const url = URL_BASE + 'clans/%23' + tag + '/capitalraidseasons?limit=1'
+    console.log({url});
+    
+    const response = await fetch(url, {
+      headers: {
+        'Authorization': `Bearer ${API_KEY}`,
+      }
+    })
+    if (!response.ok) {
+      throw new Error(`Error en la API: ${response.status}`)
+    }
+    const data = await response.json()
+    res.json(data)
   }catch (error) {
     res.status(500).json({ error: error.message });
   }
-  
 })
 
 app.listen(port, () => {
